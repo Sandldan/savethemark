c9cf7547389348ab0c13d5c6ebd8f417642ddea1
Daniel Wärnå <daniel.warna@gmail.com>
2013-04-20 10:53:50 +0300

    Bookmark class

---
+++ savethemark/src/main/java/foo/Bookmark.java
@@ -0,0 +1,53 @@
+package foo;
+
+import com.vaadin.ui.Button;
+import com.vaadin.ui.HorizontalLayout;
+
+public class Bookmark extends HorizontalLayout {
+
+	private String description;
+	private int position; 
+	
+	private Button bookmarkButton;
+	private Button deletButton;
+	
+	public Bookmark(int position, String description) {
+		this.position = position;
+		this.description = description;
+		
+		bookmarkButton = new Button(description);
+		deletButton = new Button("delete");
+		
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
+	}
+
+	public Button getBookmarkButton() {
+		return bookmarkButton;
+	}
+
+	public Button getDeletButton() {
+		return deletButton;
+	}
+
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
